4e82e60ef96b707a6270cac26b3afb0c71c92ef6
mahdi <mehdi.bagvand@gmail.com>
2017-01-28 00:03:35 +0330

    news update implemented

--- saveToCouchdb.py
+++ saveToCouchdb.py
@@ -12,93 +12,102 @@ db = couch['news']
 for root, dirs, files in os.walk('news/', topdown=False):
     for name in files:
         print(os.path.join(root, name))
-        file = open(os.path.join(root, name), 'r')
-        array_news = json.loads(file.read(), "utf8")
+        one_file = open(os.path.join(root, name), 'r')
+        array_news = json.loads(one_file.read(), "utf8")
         for news in array_news:
-            id = news['id']
+            _id = news['id']
             del news['id']
-            if id not in db:
-                db[id] = news
+            if _id not in db:
+                db[_id] = news
+            else:
+                is_update_exist = False
+                news_inDb = db[_id]
+                for key in news.keys():
+                    if news[key] != news_inDb.get(key):
+                        news_inDb[key] = news[key]
+                        is_update_exist = True
+                        print news
+
+                if is_update_exist:
+                    db[news_inDb.id] = news_inDb
 
 
+                    # couchdbb=couchdb
+                    # couchdbi=couchdbb.client.Server('http://admin:admin@127.0.0.1:5984')
 
 
-            # couchdbb=couchdb
-            # couchdbi=couchdbb.client.Server('http://admin:admin@127.0.0.1:5984')
+                    # db=couchdbi.create['testt']
+                    # db=couchdbi['testt']
+                    # print("the data base name is "+db.name)
 
+                    # doc_id ,doc_rev=db.save({'type':'person','name':'ali'})
+                    # doc=db[doc_id]
+                    # doc2={'name':'arash/ali22','type':'person'}
+                    # db.save(doc2)
 
-            # db=couchdbi.create['testt']
-            # db=couchdbi['testt']
-            # print("the data base name is "+db.name)
+                    # print("_________________")
+                    # print(doc)
+                    # print("_________________")
 
-            # doc_id ,doc_rev=db.save({'type':'person','name':'ali'})
-            # doc=db[doc_id]
-            # doc2={'name':'arash/ali22','type':'person'}
-            # db.save(doc2)
+                    # print(doc['type'])
+                    # print(doc['name'])
+                    # del db[doc.id]
+                    # print(doc.id in db)
 
-            # print("_________________")
-            # print(doc)
-            # print("_________________")
+                    # print("tasks is")
+                    # print(couchdbi.tasks())
 
-            # print(doc['type'])
-            # print(doc['name'])
-            # del db[doc.id]
-            # print(doc.id in db)
+                    # print("couchdb version")
+                    # print(couchdbi.version())
 
-            # print("tasks is")
-            # print(couchdbi.tasks())
+                    # print(db.commit())
 
-            # print("couchdb version")
-            # print(couchdbi.version())
+                    # print("add a doc with client uuid generator")
+                    # doc_id3=uuid4().hex
+                    # db[doc_id3]={'type':'person','name':'farnaz'}
 
-            # print(db.commit())
+                    # dos=dict(type='person',name='arash')
+                    # db['arash22']=dos
+                    # dos2=db['arash']
+                    # dos2['age']=22
+                    # db['arash']=dos2
+                    # dos=db['arash']
+                    # db.delete(dos)
 
-            # print("add a doc with client uuid generator")
-            # doc_id3=uuid4().hex
-            # db[doc_id3]={'type':'person','name':'farnaz'}
+                    # db.delete_attachment('arash','customerchurn.png')
 
-            # dos=dict(type='person',name='arash')
-            # db['arash22']=dos
-            # dos2=db['arash']
-            # dos2['age']=22
-            # db['arash']=dos2
-            # dos=db['arash']
-            # db.delete(dos)
+                    # import image
+                    # img=image.open('C:\Users\Programmer\Pictures\FootBall\0ef52a30-dc1c-11e3-8dcb-21ab1f7130ff_portugal_slogan.jpg')
+                    # img.show()
 
-            # db.delete_attachment('arash','customerchurn.png')
+                    # couchdbi.add_user('arashi','123')
+                    # to=couchdbi.login('arash','123')
+                    # print("User Token       "+to)
+                    # print(couchdbi.verify_token(to))
+                    # couchdbi.logout(to)
+                    # print(couchdbi.verify_token(to))
 
-            # import image
-            # img=image.open('C:\Users\Programmer\Pictures\FootBall\0ef52a30-dc1c-11e3-8dcb-21ab1f7130ff_portugal_slogan.jpg')
-            # img.show()
+                    # doc={'foo':'bar'}
+                    # db.save(doc)
+                    # couchdbi.login('arash','123')
+                    # print(couchdbi.stats())
+                    # for id in db:
+                    #   tdoc=db[id]
+                    #  if (tdoc['name']=='arash/ali22'):
+                    #        tdoc['name']='arash/ali223'
+                    #        tdoc['edu']='arshad'
+                    #        db[tdoc.id]=tdoc
+                    #        del db[tdoc.id]
+                    #    print(tdoc['name']+"   "+tdoc['type'])
 
-            # couchdbi.add_user('arashi','123')
-            # to=couchdbi.login('arash','123')
-            # print("User Token       "+to)
-            # print(couchdbi.verify_token(to))
-            # couchdbi.logout(to)
-            # print(couchdbi.verify_token(to))
-
-            # doc={'foo':'bar'}
-            # db.save(doc)
-            # couchdbi.login('arash','123')
-            # print(couchdbi.stats())
-            # for id in db:
-            #   tdoc=db[id]
-            #  if (tdoc['name']=='arash/ali22'):
-            #        tdoc['name']='arash/ali223'
-            #        tdoc['edu']='arshad'
-            #        db[tdoc.id]=tdoc
-            #        del db[tdoc.id]
-            #    print(tdoc['name']+"   "+tdoc['type'])
-
-            # map_fun = '''function(doc){
-            #        if(doc.type=='person')
-            #            emit(doc.type,doc.name);
-            #    }'''
-            #    map_reduce='''function(key,values,rereduce){
-            #   return values.length;
-            # }'''
-            #    for row in db.query(map_fun,map_reduce):
-            #        print(row['key'])#row.key
-            #        print(row['value'])#row.value
-            # break
+                    # map_fun = '''function(doc){
+                    #        if(doc.type=='person')
+                    #            emit(doc.type,doc.name);
+                    #    }'''
+                    #    map_reduce='''function(key,values,rereduce){
+                    #   return values.length;
+                    # }'''
+                    #    for row in db.query(map_fun,map_reduce):
+                    #        print(row['key'])#row.key
+                    #        print(row['value'])#row.value
+                    # break
